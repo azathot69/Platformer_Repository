4741c0c6dc1d4da2b538c30d79f69e613eb8da94
azathot69 <jacuna98@gmail.com>
2023-10-24 23:10:00 -0700

    Updated "Player Control" Functions
    Updated the script that allows the player to move around, jump, and respawn after falling off the platform. He still can't attack yet. Also added the color black

--- Platformer_Group_Project/Assets/Scripts/PlayerControl.cs
+++ Platformer_Group_Project/Assets/Scripts/PlayerControl.cs
@@ -14,27 +14,42 @@ public class PlayerControl : MonoBehaviour
     //Variables
     private int lives = 3;
     private int points = 0;
+    public int speed = 5;
+    private Rigidbody rigidbodyRef;
+    public float jumpForce = 10f;
+    public float deathYLevel = -2;
 
     private bool attacking = false;
     private float attackRate = 1.3f;
-    private float deathFall = 0f;
+
+    public Vector3 startingX;
+    public Vector3 startingY;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Get rigidbody component off of the object & stores a reference to it
+        rigidbodyRef = GetComponent<Rigidbody>();
+
+        //Store object's X coordinates when scene starts
+        startingX = transform.position;
+        startingY = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Check input for movement
-
+        Movement();
         //Check input for Jump
 
         //Check input for attack
 
         //Check for pitfall
+        if (transform.position.y <= deathYLevel)
+        {
+            Respawn();
+        }
     }
 
     //Functions
@@ -58,7 +73,34 @@ public class PlayerControl : MonoBehaviour
     /// </summary>
     private void Movement()
     {
+        //Movement
+        if (Input.GetKey(KeyCode.A) || Input.GetKey("left"))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey("right"))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W) || Input.GetKey("up"))
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey("down"))
+        {
+            transform.position -= Vector3.forward * speed * Time.deltaTime;
+        }
 
+        //Controls
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            HandleJump();
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Debug.Log("Now Attacking");
+        }
     }
 
     /// <summary>
@@ -66,24 +108,62 @@ public class PlayerControl : MonoBehaviour
     /// </summary>
     private void HandleJump()
     {
+        RaycastHit hit;
 
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 1.3f))
+        {
+            Debug.Log("Player is touching the ground. Can Jump");
+            rigidbodyRef.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
     }
 
     /// <summary>
     /// MaMakes the player lose a life.
     /// If there is no lives left, perish
     /// </summary>
-    private void Hurt()
+    private void Respawn()
     {
         if (lives != 0)
         {
             lives--;
-            
+
             //Return to last spawn point
+            transform.position = startingX;
+            transform.position = startingY;
         }
         else
         {
             Debug.Log("The player has died");
+
+            //Replace below code with proper Game Over Scene
+            transform.position = startingX;
+            transform.position = startingY;
+        }
+    }
+
+    /// <summary>
+    /// Deals with collision
+    /// </summary>
+    /// <param name="other">The object being collided with</param>
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.gameObject.tag)
+        {
+            default:
+                Debug.Log("If you see this, let me know - Joseph");
+                break;
+
+            case "Enemy":
+                Respawn();
+                break;
+
+            case "Points":
+                //add to points
+                break;
+
+            case "Lives":
+                //add to lives
+                break;
         }
     }
 }
