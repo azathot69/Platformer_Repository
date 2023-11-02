8819b17063b72cef44e84de2f16f3b276c2e8232
Jepore <jasperliebert@gmail.com>
2023-11-02 14:32:24 -0700

    Crate drops wumpa fruits and spreads them out

--- Platformer_Group_Project/Assets/Scripts/Crate.cs
+++ Platformer_Group_Project/Assets/Scripts/Crate.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 public class Crate : MonoBehaviour
 {
     Vector3 temp = Vector3.up;
+    public GameObject wumpPrefab;
 
     private float topY;
     private float bottomY;
@@ -24,8 +25,12 @@ public class Crate : MonoBehaviour
     void Update()
     {
         Wiggle();
+        Smash(5);
     }
 
+    /// <summary>
+    /// This will make the crate bob up and down 
+    /// </summary>
     public void Wiggle()
     {
         if (goingUp)
@@ -45,5 +50,40 @@ public class Crate : MonoBehaviour
             }
         }
         transform.position += temp * crateSpeed * Time.deltaTime;
+    }
+
+    IEnumerator Death()
+    {
+        yield return new WaitForSeconds(5.0f);
+        Smash(1);
+    }
+
+    /// <summary>
+    /// Smashes the crate and spawns a number of wumpa fruit in random positions nearby
+    /// </summary>
+    /// <param name="wumps"></param>
+    public void Smash(int wumps)
+    {
+        for (int i=0; i<wumps; i++)
+        {
+            Instantiate(wumpPrefab, ChangePosition(this.gameObject.transform.position), transform.rotation);
+        }
+        this.gameObject.SetActive(false);
+    }
+
+    /// <summary>
+    /// takes an input vector3 and changes the position by 1 to make the wumps separate from eachother
+    /// </summary>
+    /// <param name="input"> vector 3 you are wanting to change </param>
+    /// <returns></returns>
+    public Vector3 ChangePosition(Vector3 input)
+    {
+        float x = input.x;
+        float y = input.y;
+        float z = input.z;
+        float randomX = Random.Range(x - 1, x + 1);
+        float randomZ = Random.Range(z - 1, z + 1);
+        temp = new Vector3(randomX, y, randomZ);
+        return temp;
     }
 }
